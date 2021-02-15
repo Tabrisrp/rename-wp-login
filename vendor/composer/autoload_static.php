d30cd888623c17a2e0f3d66bf5be025eeb7dd374
nkulka <nkulka@beapi.fr>
2021-02-15 12:28:04 +0100

    update composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,7 +9,7 @@ class ComposerStaticInitaaeeb2759878cb59c7a1ebca78d83d93
     public static $files = array (
         '75114bb77681cceeacec8e9fdc25fe5f' => __DIR__ . '/..' . '/nicolaskulka/wp-dismissible-notices-handler/handler.php',
         '02e78d03a068280c2da71f3d013fe5e3' => __DIR__ . '/..' . '/nicolaskulka/wp-dismissible-notices-handler/includes/helper-functions.php',
-        '642f204a71b38c9724089b10b2126d4f' => __DIR__ . '/..' . '/NicolasKulka/wp-review-me/review.php',
+        '642f204a71b38c9724089b10b2126d4f' => __DIR__ . '/..' . '/nicolaskulka/wp-review-me/review.php',
     );
 
     public static $classMap = array (
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,19 +6,19 @@
     'aliases' => 
     array (
     ),
-    'reference' => '9e9fbdbd77ba759f2e97f97c4c2e9d65dc104f3e',
+    'reference' => '060859bbb63c73b234850531410bb9bd092ab512',
     'name' => 'wpserveur/wps-hide-login',
   ),
   'versions' => 
   array (
     'nicolaskulka/wp-dismissible-notices-handler' => 
     array (
-      'pretty_version' => '2.1',
-      'version' => '2.1.0.0',
+      'pretty_version' => '2.2',
+      'version' => '2.2.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'a7cb98c12161867b9c5160d28204a5266b8d8814',
+      'reference' => '7257ae7f245aedbd3cba6e0f5a7d8bdd49210f4a',
     ),
     'nicolaskulka/wp-review-me' => 
     array (
@@ -27,7 +27,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '80e0bd652c87d226c3ac3e0bc0f731f2d6cd8214',
+      'reference' => 'a9737a9a4e54c90715c0ca151042e3f4404fafde',
     ),
     'wpserveur/wps-hide-login' => 
     array (
@@ -36,7 +36,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '9e9fbdbd77ba759f2e97f97c4c2e9d65dc104f3e',
+      'reference' => '060859bbb63c73b234850531410bb9bd092ab512',
     ),
   ),
 );
